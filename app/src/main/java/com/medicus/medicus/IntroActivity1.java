788a9c7eead2d106f1d2bec4edb9c1d5d08f782d
kushbhardwaj28 <phenixxxx0328@gmail.com>
2018-02-14 17:46:19 +0530

    fixed intro screen and splash screen

--- app/src/main/java/com/medicus/medicus/IntroActivity1.java
+++ app/src/main/java/com/medicus/medicus/IntroActivity1.java
@@ -1,7 +1,9 @@
 package com.medicus.medicus;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -13,14 +15,24 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntroActivity1 extends AppCompatActivity {
 
@@ -32,14 +44,21 @@ public class IntroActivity1 extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-    private IntroActivity1.SectionsPagerAdapter mSectionsPagerAdapter;
+//    private IntroActivity1.SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private final Handler mHideHandler = new Handler();
     private ViewPager mViewPager;
-    
+    String str_device;
+    MyPageAdapter obj_adapter;
+
+    RelativeLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
+
+
 
 
     @Override
@@ -47,13 +66,7 @@ public class IntroActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro1);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new IntroActivity1.SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        init();
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -71,104 +84,79 @@ public class IntroActivity1 extends AppCompatActivity {
             }
         });
 
+
+
+
+
+
     }
 
     public void changeInstance(){
-        Intent main = new Intent(IntroActivity1.this,
-                MainActivity.class);
-        IntroActivity1.this.startActivity(main);
-        IntroActivity1.this.finish();
+//        Intent main = new Intent(IntroActivity1.this,
+//                ScreenSlidePagerActivity.class);
+//        IntroActivity1.this.startActivity(main);
+//        IntroActivity1.this.finish();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_intro, menu);
 
-        return true;
+
+    private void init() {
+        mViewPager = (ViewPager) findViewById(R.id.container);
+//        differentDensityAndScreenSize(getApplicationContext());
+        List<Fragment> fragments = getFragments();
+        mViewPager.setAdapter(obj_adapter);
+        mViewPager.setClipToPadding(false);
+
+
+
+        obj_adapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+        mViewPager.setPageTransformer(true, new ExpandingViewPagerTransformer());
+        mViewPager.setAdapter(obj_adapter);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    class MyPageAdapter extends FragmentPagerAdapter {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        private List<Fragment> fragments;
 
-        return super.onOptionsItemSelected(item);
-    }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static IntroActivity.PlaceholderFragment newInstance(int sectionNumber) {
-            IntroActivity.PlaceholderFragment fragment = new IntroActivity.PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = null;
-            switch(getArguments().getInt(ARG_SECTION_NUMBER)){
-                case 1:
-                    rootView = inflater.inflate(R.layout.fragment_intro, container, false);
-                    break;
-                case 2:
-                    rootView = inflater.inflate(R.layout.fragment_intro_tile1, container, false);
-                    break;
-                case 3:
-                    rootView = inflater.inflate(R.layout.fragment_intro_tile2, container, false);
-                    break;
-            }
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
+
+            this.fragments = fragments;
+
         }
 
         @Override
+
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return IntroActivity.PlaceholderFragment.newInstance(position + 1);
+
+            return this.fragments.get(position);
+
         }
 
         @Override
+
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+
+            return this.fragments.size();
+
         }
+
     }
+
+    private List<Fragment> getFragments() {
+
+        List<Fragment> fList = new ArrayList<Fragment>();
+
+        fList.add(MyFragment.newInstance("Need a Doctor?","Search doctor using the app and find the best doctor near you.", R.mipmap.ic_launcher_round));
+        fList.add(MyFragment.newInstance("Make & Manage Appointment","Make and Manage appointments in a single app.", R.mipmap.ic_launcher_round));
+        fList.add(MyFragment.newInstance("Meet your Doctor", "Meet your doctor & never miss your appointment.",R.mipmap.ic_launcher_round));
+
+        return fList;
+
+    }
+
+
 }
